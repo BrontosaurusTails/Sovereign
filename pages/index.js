07a56bec04e662d9fc9f6756af6c1e2752774f58
Yeiber Cano <cano@adobe.com>
2017-07-18 16:05:40 -0700

    add footer to landing page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import Footer from '../components/footer';
 
 export default () => (
-  <div>Hello World!</div>
+  <div>
+    <p>Hello World!</p>
+    <Footer />
+  </div>
 );
